6b6f32cbb9e01fd0c79a6046dc6329b52c5670b4
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-08-18 08:59:55 +0000

    Migrate on startup

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,14 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = mysql.createPool(env.DATABASE_URL);
+export const client = mysql.createPool(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema, mode: 'default' });
+
+await migrate(db, {
+    migrationsFolder: './drizzle'
+});
