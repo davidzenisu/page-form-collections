4c2ccec9ed616dd0f597449c9febcadb98578dfc
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-09-07 14:18:39 +0000

    Add events to landing page

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	let { data } = $props();
+
+	const dateOptions = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		timeZone: 'UTC'
+	};
+</script>
+
+<div>
+	<h1>Upcoming events!</h1>
+	<ul class="todos">
+		{#each data.events as event}
+			<span>{event.name}</span>
+			<span>{event.time.toLocaleString('en-GB', dateOptions)}</span>
+		{/each}
+	</ul>
+</div>
