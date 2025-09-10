75266df5c48d0e8dac96b847be03360a4ba6f039
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-09-10 20:11:31 +0000

    Fix top level await constraint

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,6 +1,5 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
-import { sha256 } from '@oslojs/crypto/sha2';
 import { encodeBase64url, encodeHexLowerCase } from '@oslojs/encoding';
 import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
