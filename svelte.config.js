cceb04cc6b3c024b069990caf3d08ca8cc07b87c
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-09-10 20:10:06 +0000

    Set Azure Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import azure from 'svelte-adapter-azure-swa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { adapter: azure() },
 	extensions: ['.svelte', '.svx']
 };
 
