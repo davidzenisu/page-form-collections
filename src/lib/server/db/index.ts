356fb2037883b007fb416e1ba2fc128cd9b0b9db
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-09-11 17:35:43 +0000

    Set dynamic private env

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,17 +1,19 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
-if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-export const client = mysql.createPool(DATABASE_URL);
+export const client = mysql.createPool(
+	env.DATABASE_URL || 'mysql://user:password@host:3307/db-name'
+);
 
 export const db = drizzle(client, { schema, mode: 'default' });
 
 export const migrateDb = async function migrateDb() {
-    return await migrate(db, {
-        migrationsFolder: './drizzle'
-    });
-}
+	if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+	return await migrate(db, {
+		migrationsFolder: './drizzle'
+	});
+};
