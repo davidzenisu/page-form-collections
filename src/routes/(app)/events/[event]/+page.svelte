bf02df77d397349a90c09da10eaa44f771b04b10
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2025-09-17 19:37:09 +0000

    Add images

--- src/routes/(app)/events/[event]/+page.svelte
+++ src/routes/(app)/events/[event]/+page.svelte
@@ -1,5 +1,17 @@
 <script>
 	let { data } = $props();
+	import eventImage from '$lib/images/main_event.jpg';
 </script>
 
-<div>{data.event.name}</div>
+<div class="flex flex-col items-center gap-3">
+	<h1>{data.event.name}</h1>
+	<div>{data.event.description}</div>
+	<div>
+		{data.event.time.toLocaleString('en-GB', {
+			dateStyle: 'full',
+			timeStyle: 'short',
+			timeZone: 'UTC'
+		})}
+	</div>
+	<img src={eventImage} alt={data.event.name} class="my-4 rounded-md" />
+</div>
